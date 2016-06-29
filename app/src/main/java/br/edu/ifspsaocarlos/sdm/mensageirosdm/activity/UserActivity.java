ae39df2f1760a077d9ee5353b1581639c5dc0242
Rafael Peratello <Rafael Peratello>
2016-06-28 23:13:12 -0300

    user activity with volley

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/activity/UserActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/activity/UserActivity.java
@@ -3,33 +3,27 @@ package br.edu.ifspsaocarlos.sdm.mensageirosdm.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import br.edu.ifspsaocarlos.sdm.mensageirosdm.R;
+import br.edu.ifspsaocarlos.sdm.mensageirosdm.network.VolleyHelper;
+import br.edu.ifspsaocarlos.sdm.mensageirosdm.util.Constants;
+import br.edu.ifspsaocarlos.sdm.mensageirosdm.util.Helpers;
 
 public class UserActivity extends AppCompatActivity {
-    private final String PREFERENCES_KEY = "PREFERENCES";
-    private final String USER_KEY = "USER";
-
-    private SharedPreferences sharedpreferences;
-
     private EditText editName;
     private EditText editNickname;
     private Button buttonSave;
@@ -39,7 +33,6 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        sharedpreferences = getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
 
         editName = (EditText) findViewById(R.id.edit_name);
         editNickname = (EditText) findViewById(R.id.edit_nickname);
@@ -48,7 +41,7 @@ public class UserActivity extends AppCompatActivity {
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveUser();
+                checkUserInfo();
             }
         });
 
@@ -56,21 +49,42 @@ public class UserActivity extends AppCompatActivity {
     }
 
     private void checkUser() {
-        if (sharedpreferences.contains(USER_KEY)) {
+        String userId = Helpers.getUserId(this);
+
+        if (!TextUtils.isEmpty(userId)) {
             showHome();
         }
     }
 
     private void showHome() {
         Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
-    private void saveUser() {
+    private void checkUserInfo() {
         String name = editName.getText().toString();
         String nickname = editNickname.getText().toString();
 
+        if ((TextUtils.isEmpty(name)) || (TextUtils.isEmpty(nickname))) {
+            Helpers.showDialog(this, R.string.dialog_content_error_invalid);
+            return;
+        }
+
+        saveUser(name, nickname);
+    }
+
+    private void saveUserOnPreferences(String id) {
+        SharedPreferences sharedpreferences = getSharedPreferences(Constants.PREFERENCES_KEY, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Constants.USER_KEY, id);
+        editor.commit();
+
+        showHome();
+    }
+
+    private void saveUser(String name, String nickname) {
         JSONObject jsonObject = new JSONObject();
 
         try {
@@ -80,67 +94,31 @@ public class UserActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        SaveUsersTask saveUsersTask = new SaveUsersTask();
-        saveUsersTask.execute(jsonObject);
-    }
+        JsonObjectRequest request = new JsonObjectRequest
+                (Request.Method.POST, Constants.SERVER_URL + Constants.CONTATO_PATH, jsonObject, new Response.Listener<JSONObject>() {
 
-    private void saveUserOnPreferences(String id) {
-        SharedPreferences.Editor editor = sharedpreferences.edit();
-        editor.putString(USER_KEY, id);
-        editor.commit();
-    }
+                    @Override
+                    public void onResponse(JSONObject json) {
+                        try {
+                            String id = json.getString("id");
 
-    private class SaveUsersTask extends AsyncTask<JSONObject, Void, String> {
+                            if ((!TextUtils.isEmpty(id)) && (!"0".equals(id)))
+                                saveUserOnPreferences(id);
+                            else
+                                Helpers.showDialog(UserActivity.this, R.string.dialog_content_error_saving_profile);
 
-        @Override
-        protected String doInBackground(JSONObject... params) {
-            StringBuilder sb = new StringBuilder();
-
-            try {
-                HttpURLConnection conexao = (HttpURLConnection) (new URL("http://www.nobile.pro.br/sdm/mensageiro/contato")).openConnection();
-                conexao.setRequestMethod("POST");
-                conexao.setDoOutput(true);
-
-                if (params[0] != null) {
-                    OutputStreamWriter wr = new OutputStreamWriter(conexao.getOutputStream());
-                    wr.write(params[0].toString());
-                    wr.flush();
-                    wr.close();
-                }
-
-                if (conexao.getResponseCode() == HttpURLConnection.HTTP_OK) {
-
-                    InputStream is = conexao.getInputStream();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                    String temp;
-
-                    while ((temp = br.readLine()) != null) {
-                        sb.append(temp);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
+                }, new Response.ErrorListener() {
 
-                    Log.e("SDM", sb.toString());
-                }
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Helpers.showDialog(UserActivity.this, R.string.dialog_content_error_saving_profile);
+                    }
+                });
 
-            } catch (IOException ioe) {
-                Log.e("SDM", "Erro na recuperação de texto");
-            }
-
-            return sb.toString();
-        }
-
-        @Override
-        protected void onPostExecute(String data) {
-            JSONObject json = null;
-            try {
-                json = new JSONObject(data);
-
-                if (data.contains("id")) {
-                    saveUserOnPreferences(json.getString("id"));
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
+        VolleyHelper.getInstance(this).addToRequestQueue(request);
     }
 }
