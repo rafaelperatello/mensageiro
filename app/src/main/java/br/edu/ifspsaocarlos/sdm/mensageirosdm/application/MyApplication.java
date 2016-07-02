de4b1864dcbe9d7524bd437f2fc5b6b8e4aeb6fe
Rafael Peratello <rafaelperatello@gmail.com>
2016-07-01 21:19:35 -0300

    move application to another package

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/application/MyApplication.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/application/MyApplication.java
@@ -1,4 +1,4 @@
-package br.edu.ifspsaocarlos.sdm.mensageirosdm.model;
+package br.edu.ifspsaocarlos.sdm.mensageirosdm.application;
 
 import android.app.Application;
 
@@ -12,6 +12,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this).build();
         Realm.setDefaultConfiguration(realmConfiguration);
     }
