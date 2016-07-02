56fe5540f1a625ffb116837782dddda90407e443
Rafael Peratello <rafaelperatello@gmail.com>
2016-07-02 17:09:33 -0300

    style message

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/adapter/MessageAdapter.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/adapter/MessageAdapter.java
@@ -2,9 +2,11 @@ package br.edu.ifspsaocarlos.sdm.mensageirosdm.adapter;
 
 
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -35,18 +37,25 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
         Message message = messageList.get(position);
         holder.mTextViewMessage.setText(message.getCorpo());
 
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) holder.mTextViewMessage.getLayoutParams();
+        RelativeLayout.LayoutParams paramsRelative = (RelativeLayout.LayoutParams) holder.mRelativeLayoutMessage.getLayoutParams();
 
         if (message.getOrigem_id().equals(userId)) {
-            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            params.addRule(RelativeLayout.ALIGN_PARENT_END);
-            params.removeRule(RelativeLayout.ALIGN_PARENT_LEFT);
-            params.removeRule(RelativeLayout.ALIGN_PARENT_START);
+            paramsRelative.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            paramsRelative.addRule(RelativeLayout.ALIGN_PARENT_END);
+            paramsRelative.removeRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            paramsRelative.removeRule(RelativeLayout.ALIGN_PARENT_START);
+
+            holder.mTextViewMessage.setGravity(Gravity.RIGHT);
+            holder.mTextViewMessage.setBackgroundResource(R.drawable.message_sent);
+
         } else {
-            params.removeRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            params.removeRule(RelativeLayout.ALIGN_PARENT_END);
-            params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-            params.addRule(RelativeLayout.ALIGN_PARENT_START);
+            paramsRelative.removeRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            paramsRelative.removeRule(RelativeLayout.ALIGN_PARENT_END);
+            paramsRelative.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            paramsRelative.addRule(RelativeLayout.ALIGN_PARENT_START);
+
+            holder.mTextViewMessage.setGravity(Gravity.LEFT);
+            holder.mTextViewMessage.setBackgroundResource(R.drawable.message_received);
         }
     }
 
@@ -61,10 +70,12 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView mTextViewMessage;
+        public RelativeLayout mRelativeLayoutMessage;
 
         public ViewHolder(View v) {
             super(v);
             mTextViewMessage = (TextView) v.findViewById(R.id.text_message);
+            mRelativeLayoutMessage = (RelativeLayout) v.findViewById(R.id.relative_message_container);
         }
     }
 }
