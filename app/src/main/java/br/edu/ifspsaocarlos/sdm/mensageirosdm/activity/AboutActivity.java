609fd020f635a578ab84fb8be7b0da808ed7d2d9
Rafael Peratello <rafaelperatello@gmail.com>
2016-07-09 15:50:56 -0300

    change styles at about

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/activity/AboutActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/mensageirosdm/activity/AboutActivity.java
@@ -14,5 +14,10 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // setup toolBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setElevation(0f);
     }
 }
